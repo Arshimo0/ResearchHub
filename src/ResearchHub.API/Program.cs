5f059dce53541c4aac29cf819a8ae5e4c8cee4d5
Arshimo0 <arshi.mosi@gmail.com>
2026-09-18 20:09:00 +0330

    feat: register DbContext and wire connection string via user secrets

--- src/ResearchHub.API/Program.cs
+++ src/ResearchHub.API/Program.cs
@@ -1,8 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ResearchHub.Infrastructure.Persistence;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ResearchHubDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ResearchHubDb")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
